6b569455480edf6613c99ab3454067859bc8ca71
Aman-piyush <136111983+Amanpiyush@users.noreply.github.com>
2025-04-22 18:01:45 +0530

    Create Variable.sh
    variable

---
+++ Variable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Using the variables
+#VAR_NAME = Value
+a=10
+b=20
+Name="Hiro"
+Age=22
+sum=$((a+b))
+echo "sum is $sum"
+echo "My name is $Name and Age is $Age"
+#VAR_NAME = ${NAME}
+#echo ${Name}
+
+#Sometime it go wrong if you provide the space. see the table
